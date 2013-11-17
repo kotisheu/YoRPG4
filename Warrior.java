8ca7730e2570e64aa88ec30bae6f604320cbfe95
andylstuy <andyl.stuy@gmail.com>
2013-11-17 14:37:14 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,20 +1,20 @@
 public class Warrior extends Character{
 
     public Warrior() {
-	name = "grimReaper";
-	HP = 150;
-	MP = 50;
-	strength = 100;
-	defense = 75;
-	attack = 0.6;
-	speed = 40;
-	smarts = 20;
-	accuracy = 0.7;
+	_name = "grimReaper";
+	_hp = 150;
+	_mp = 50;
+	_strength = 100;
+	_defense = 75;
+	_attack = 0.6;
+	_speed = 40;
+	_smarts = 20;
+	_accuracy = 0.7;
     }
 
     public Warrior(String s) {
 	this();
-	name = s;
+	_name = s;
     }
  
 }
