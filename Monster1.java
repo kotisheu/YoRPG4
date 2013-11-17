7c2b8f1fd0d0292b873c1ffb56cfe3d0f0823598
andylstuy <andyl.stuy@gmail.com>
2013-11-17 14:24:06 -0500

    Update Monster1.java

--- Monster1.java
+++ Monster1.java
@@ -1,12 +1,12 @@
 public class Monster1 extends Character {
 
-    private String name;
+    private String _name;
 
     public Monster1() {
-	    health = 150;
-	    defense = 20;
-	    attackRating = 1;
-	    strength = (int) (20 + (45 * Math.random()));
+	    _health = 150;
+	    _defense = 20;
+	    _attack = 1;
+	    _strength = (int) (20 + (45 * Math.random()));
     }
 
     public void specialize() {
