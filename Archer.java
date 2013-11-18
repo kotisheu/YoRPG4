b882681bca135e2e317e38f7d147bcfa6bb509ed
emilyliao97 <liao.emily022@gmail.com>
2013-11-17 22:19:28 -0500

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -24,4 +24,9 @@ public class Archer extends Character {
     _smarts *= 1.2;
   }
   
+  public string about() {
+    String desc = "";
+    desc = "A lighthearted lad with 20/20 vision that can impale foes from faraway distances with his bow and arrows."
+    return desc;
+  }
 }
