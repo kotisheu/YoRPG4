2cd158983c21835b0b8c819a35853b5c6eb2307d
emilyliao97 <liao.emily022@gmail.com>
2013-11-17 22:13:28 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,27 +1,30 @@
 public class Warrior extends Character{
 
-    public Warrior() {
-	_name = "grimReaper";
-	_hp = 150;
-	_mp = 50;
-	_strength = 100;
-	_defense = 75;
-	_attack = 0.6;
-	_speed = 40;
-	_smarts = 20;
-	_accuracy = 0.7;
-    }
+    	public Warrior() {
+		_name = "grimReaper";
+		_hp = 150;
+		_mp = 50;
+		_strength = 100;
+		_defense = 75;
+		_attack = 0.6;
+		_speed = 40;
+		_smarts = 20;
+		_accuracy = 0.7;
+    	}
 
-    public Warrior(String s) {
-	this();
-	_name = s;
-    }
+    	public Warrior(String s) {
+		this();
+		_name = s;
+    	}	
 
-public void levelUp() {
-    _hp *= 1.2;
-    _mp *= 1.2;
-    _defense *= 1.2;
-    _smarts *= 1.2;
-  }
+	public void levelUp() {
+    		_hp += 50;
+		 _mp += 5;
+  	}
+  	
+  	public String about() {
+  		String desc = "A fine soul that hath a sword to slay and parry beasts that roam.";
+  		return desc;
+  	}
  
 }
