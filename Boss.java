17883ce63834a35f40df3f827df5d15ddc7088e0
Kotisheu <iiacherry@aim.com>
2013-11-18 03:02:15 -0500

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -2,15 +2,21 @@ public class Boss extends Character {
 
     private String _name;
 
-    public Boss() {
+    public Boss(Character c) {
     		_name= "Boss";	
-	    _hp = 250;
-	    _defense = 30;
-	    _attack = 1.5;
-	    _strength = (int) (20 + (65 * Math.random()));
-	    _level = 1;
-    }
+	    _hp = 75*_level;
+	    _defense = _level * 15;
+	    _attack = .95;
+	    _strength = (int) (_level + (65 * Math.random()));
+	    _speed= 50;
+	    _level = c.getLevel+(int)(Math.random()*4);
+	    _exp=_level*2;
+	    _accuracy=.90;
+    }	   
 
+ public String getName(){
+ 	return _name;
+ }
   public String getName(){
   	return _name;
   }
