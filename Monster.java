b68ccbe96eb02d70a35bf558c62d02adc0edee59
Kotisheu <iiacherry@aim.com>
2013-11-18 00:17:43 -0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -3,22 +3,26 @@ public class Monster extends Character {
     private String _name;
 
     public Monster() {
+    		_name="minion"
 	    _hp = 150;
 	    _defense = 20;
 	    _attack = 1;
 	    _strength = (int) (20 + (45 * Math.random()));
     }
 
-    public void specialize() {
-	    ;
-    }
-
-    public void normalize() {
-    	;
+ public abstract String getName(){
+ 	return _name;
+ }
+    public abstract void normalize(){
     }
     
-    public void statsUp() {
-    	;
+    public abstract void skill(){
+    	
     }
-
+    public abstract String about(){
+    	return " The boss's little henchmen."
+    }
+    public abstract void statsUp(){
+    }
+    
 }
